e55fff54000f041dbedd530028c1a94c5abb3854
Suwan <imissyou5466@gmail.com>
2023-08-24 21:37:40 +0900

    Sign 스타일링 1차

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,20 @@ import Lion from '@/components/Lion';
 import Message from '@/components/Message/Message';
 import styles from './Home.module.css';
 import CircleButton from '@/components/CircleButton/CircleButton';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <>
-      <div>
-        <Message />
+      <div className={styles.messageContainer}>
+        <div>
+          <Message />
+        </div>
+        <Lion />
+        <Link to="/signin">
+          <CircleButton circleButtonText={'편지 쓰기'} />
+        </Link>
       </div>
-      <CircleButton circleButtonText={'편지 쓰기'} />
-      <Lion />
     </>
   );
 }
--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,10 +1,57 @@
-import styles from './SignIn.module.css'
+import Lion from '@/components/Lion';
+import styles from './SignIn.module.css';
+import { useId } from 'react';
+import CircleButton from '@/components/CircleButton/CircleButton';
 
 function SignIn() {
-       
+  const signInId = useId();
+  const signInPassword = useId();
+
   return (
     <>
-      <div>SignIn</div>
+      <Lion />
+      <form className={styles.signInForm}>
+        <h2 className={styles.title}>로그인</h2>
+        <div className={styles.inputWrapper}>
+          <div className={styles.inputInnerWrapper}>
+            <div className={`${styles.inputLabel} ${styles.inputId}`}>
+              <label htmlFor={signInId} className={styles.inputLabel}>
+                아이디
+              </label>
+              <input
+                type="text"
+                id={signInId}
+                name={signInId}
+                className={styles.input}
+                placeholder="lion1234"
+              />
+            </div>
+          </div>
+          <div className={styles.inputInnerWrapper}>
+            <label htmlFor={signInPassword} className={styles.inputLabel}>
+              비밀번호
+            </label>
+            <input
+              type="password"
+              id={signInPassword}
+              name={signInPassword}
+              className={styles.input}
+              placeholder="*********"
+            />
+          </div>
+        </div>
+        <div className={styles.helpWrapper}>
+          <span>비밀번호를 잊어버렸나요 ?</span>
+          <span className={styles.joinMembership}>회원가입</span>
+        </div>
+
+        <CircleButton
+          type={'submit'}
+          circleButtonText={'확인'}
+          width={'140px'}
+          height={'140px'}
+        />
+      </form>
     </>
   );
 }
