c06fb2350adb34d1b4e174d6c4bb3badaff82b2e
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-25 18:38:38 +0900

    머지 후 깨진 컴포넌트 수정

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -2,12 +2,14 @@ import Envelope from "./Envelope";
 
 function Letter({ text }) {
   return (
-    <div className="flex flex-col">
-      <Envelope />
-      <span className="flex justify-end self-center items-center bg-lionYellow w-[250px] h-[34px] rounded-[10px] text-[20px] font-bold pr-4">
-        From. {text}
-      </span>
-    </div>
+    <>
+      <a href="" className="flex flex-col">
+        <Envelope />
+        <span className="flex justify-end self-center items-center bg-lionYellow w-[250px] h-[34px] rounded-[10px] text-[20px] font-bold pr-4">
+          From. {text}
+        </span>
+      </a>
+    </>
   );
 }
 
