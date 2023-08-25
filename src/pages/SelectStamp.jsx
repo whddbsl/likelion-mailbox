6c90c41e9706cbde2f7386f7f13ef5e8d252c3c3
bomlang <bomlang4211@gmail.com>
2023-08-25 18:04:55 +0900

    [merge]

--- src/pages/SelectStamp.jsx
+++ src/pages/SelectStamp.jsx
@@ -1,16 +1,29 @@
 import Header from '@/layout/header';
+import { useState } from 'react';
 import { useEffect } from 'react';
 
 function SelectStamp() {
-  // console.log(Lion());
+  const [giphy, setGiphy] = useState(null);
+
   useEffect(() => {
     const key = import.meta.env.VITE_GIPHY_API_KEY;
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${key}`;
-    console.log(url);
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${key}&limit=1&q=`;
+
+    const getGiphyData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setGiphy(data);
+      console.log(giphy);
+    };
+    getGiphyData();
+    console.log(giphy);
   }, []);
+
   return (
     <>
       <Header />
+      <div></div>
     </>
   );
 }
