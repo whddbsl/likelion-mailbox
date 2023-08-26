c8a2eb6a983b445db0b27b0312c4dc9eb0333e3c
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-27 01:16:47 +0900

    메세지 정렬 수정

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -7,12 +7,12 @@ function Letter() {
   if (data) {
     return (
       <>
-        <ul className="flex gap-10">
+        <ul className="flex flex-wrap gap-6 max-w-[1280px] justify-between px-2">
           {data.items.map((item) => (
             <li key={item.id}>
               <a href="/" className="flex flex-col">
                 <Envelope />
-                <span className="flex justify-end self-center items-center bg-lionYellow w-[250px] h-[34px] rounded-[10px] text-[20px] font-bold pr-4">
+                <span className="flex justify-end self-center items-center bg-lionYellow w-[250px] h-[34px] rounded-[10px] text-[20px] font-bold pr-6">
                   From. {item.fromId}
                 </span>
               </a>
--- src/pages/ShowCardList.jsx
+++ src/pages/ShowCardList.jsx
@@ -5,7 +5,7 @@ function ShowCardList() {
   return (
     <>
       <Header text="편지가 이만큼 도착했어요!" />
-      <div className="flex flex-row gap-8 justify-center items-center my-10 mx-10 bg-red-300">
+      <div className="flex flex-row justify-self-start justify-items-start ">
         <Letter />
       </div>
     </>
