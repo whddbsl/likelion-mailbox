f97401a6f5a52e77908e2027ebd35f651c343b36
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-27 03:40:02 +0900

    편지 컬러 변경

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
 import Envelope from "./Envelope";
 import useMessageList from "@/hooks/useMessageList";
 
 function Letter() {
   const { data } = useMessageList();
+  // console.log(data.items[0].envelope);
   if (data) {
     return (
       <ul className="flex flex-wrap gap-4 max-w-[1280px] justify-between px-4">
         {data.items.map((item) => (
           <li key={item.id}>
-            <a href="/" className="flex flex-col justify-center items-center">
-              <Envelope />
+            <Link to="/" className="flex flex-col justify-center items-center">
+              <Envelope color={item.envelope} />
               <span className="flex justify-end self-center items-center bg-lionYellow w-[250px] h-[34px] rounded-[10px] text-[20px] font-bold pr-6">
                 From. {item.fromId}
               </span>
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
