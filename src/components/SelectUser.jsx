5f7cda1095482c2ce8f0bed225d9ab209b862a0a
haha41 <thgml500@gmail.com>
2023-08-27 00:38:42 +0900

    데이터 가져오기 반쪽 성공

--- src/components/SelectUser.jsx
+++ src/components/SelectUser.jsx
@@ -2,23 +2,43 @@ import useFetchData from "@/hooks/useFetchData";
 import UserList from "@/components/UserList";
 import { pb } from "@/api/pocketbase";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+
+
 
 // const record = await pb.collection('users').getFullList();
 // console.log(record)
 // console.log('pb', pb);
 
+// const endpoint = 'https://likelion-mailbox.pockethost.io/api/collections/users/records'
+const endpoint = 'https://likelion-mailbox.pockethost.io/api/collections/test/records'
+
+
 function SelectUser() {
   
-  const {data} = useFetchData(pb.collection('users'));
+  const {data} = useFetchData(endpoint);
 
-  console.log(data);
+
+  const userInform = data?.items.map((item) => (
+    console.log(item.id)
+  ))
+
+  console.log(userInform);
+  // const getUsers = () => {
+  //   return  pb.collection('test').getFullList();
+  // }
+  // console.log(getUsers());
+
+  // const {data} = useFetchData(pb.collection('users'));
+
+  // console.log(data);
 
 
   // const listId = useId();
 
-  const handleSelectUser = () => {
-    console.log('선택');
-  }
+  // const handleSelectUser = () => {
+  //   console.log('선택');
+  // }
 
 
   const userList = Array(9)
@@ -34,8 +54,8 @@ function SelectUser() {
 
       {userList.map((key, i) => {
         return (
-          <li key={key} className="w-[300px] h-[64px] rounded-[10px] bg-inputYellow flex justify-center items-center font-extrabold hover:bg-lionYellow hover:cursor-pointer" onClick={handleSelectUser}>
-          <Link to={`/selectStamp`}>{userList[i]}</Link></li>
+          <li key={key} className="w-[300px] h-[64px] rounded-[10px] bg-inputYellow flex justify-center items-center font-extrabold hover:bg-lionYellow hover:cursor-pointer" >
+          <Link to="/selectenvelope">{userList[i]}</Link></li>
         )
       })}
     </ul>
