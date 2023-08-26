28a6fc8aa55e31fb2de0eb692721f17b681b2ef4
haha41 <thgml500@gmail.com>
2023-08-27 05:41:06 +0900

    엔터 눌렀을 때 초기화 되지 않도록

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,28 +4,34 @@ import { useId } from "react";
 import SearchButton from "./SearchButton";
 
 function SearchBar() {
-
-  const searchId = useId();
-  const [searchInput, setUserInput] = useState();
   
-  //# 검색창
+  const searchId = useId();
+  const [searchInput, setUserInput] = useState("");
+  
+  // 검색창
   const handleChangeInput = debounce((e) => {
     setUserInput(e.target.value.toLowerCase())
   }, 300);
 
+  // 엔터 눌렀을 때 초기화 되지 않도록
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <form className=" flex items-center justify-center py-10">
+      <form onSubmit={handleSubmit} className=" flex items-center justify-center py-10">
         <label htmlFor={searchId} className="sr-only">검색</label>
         <input 
           type="text"
           name="search"
           id={searchId}
+          placeholder="   검색어를 입력하세요"
           defaultValue={searchInput}
           onChange={handleChangeInput}
           className="w-[993px] h-[57px] rounded-[10px] border-2 border-solid border-lionBlack bg-lionWhite mr-2 outline-lionYellow"
         />
-      <SearchButton searchInput={searchInput}/>
+        <SearchButton searchInput={searchInput}/>
       </form>
     </>
   )
