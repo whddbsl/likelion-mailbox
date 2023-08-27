b55fce2e7631d72155767db7961d3c6c49a537c4
whddbsl <whddbs04140@naver.com>
2023-08-27 21:08:58 +0900

    경로 수정

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 function CircleButton({
+
   type,
   circleButtonText,
   width,
@@ -13,16 +15,18 @@ function CircleButton({
 
   return (
     <>
-      <button
-        className={`bg-lionYellow ${borderWidth} border-black rounded-full cursor-pointer}`}
-        type={type}
-        style={circleButtonSize}
-        onClick={handleCreateUser}
-      >
-        <span className="font-extrabold text-center text-[2.5rem]">
-          {circleButtonText}
-        </span>
-      </button>
+      <Link to='/selectpage'>
+        <button
+          className={`bg-lionYellow ${borderWidth} border-black rounded-full cursor-pointer}`}
+          type={type}
+          style={circleButtonSize}
+          onClick={handleCreateUser}
+        >
+          <span className="font-extrabold text-center text-[2.5rem]">
+            {circleButtonText}
+          </span>
+        </button>
+      </Link>
     </>
   );
 }
