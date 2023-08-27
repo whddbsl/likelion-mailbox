f81b4f23d4e9497188eda116765dc7bb18e19304
haha41 <thgml500@gmail.com>
2023-08-27 15:51:37 +0900

    타이틀 변경

--- src/pages/SelectLion.jsx
+++ src/pages/SelectLion.jsx
@@ -1,11 +1,13 @@
 import SearchBar from "@/components/SearchBar"
 import SelectUser from "@/components/SelectUser"
+import useDocumentTitle from "@/hooks/useDocumentTitle"
 import Header from "@/layout/Header"
 
 
 
 function SelectLion() {
 
+  useDocumentTitle("SelectLion")
   const text = "어떤 사자에게 편지를 남겨볼까요?"
 
   return (
