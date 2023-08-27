d37b616c411c4715dbb33d64f978574e8dec6fec
haha41 <thgml500@gmail.com>
2023-08-27 17:59:50 +0900

    사자 리본 색 변경

--- src/pages/SelectLion.jsx
+++ src/pages/SelectLion.jsx
@@ -12,7 +12,7 @@ function SelectLion() {
 
   return (
     <main className="w-[1280px] h-[832px]">
-      <Header text={"어떤 사자에게 편지를 남겨볼까요?"} lionColor={"red"} />
+      <Header text={"어떤 사자에게 편지를 남겨볼까요?"} lionColor={"red"}/>
       <SearchBar />
       <SelectUser />
     </main>
